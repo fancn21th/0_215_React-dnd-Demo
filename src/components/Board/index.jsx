0c55dec28c1ff397f42f772a5ce02f30e2f2a8b8
fancn21th <fancn21th@gmail.com>
2022-10-02 17:52:04 +0800

    drag and drop

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,29 +1,25 @@
 import React from "react";
-import { canMoveKnight, moveKnight } from "../Game";
-import Square from "../Square";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import BoardSquare from "../BoardSquare";
 import Knight from "../Knight";
 
-function handleSquareClick(toX, toY) {
-  if (canMoveKnight(toX, toY)) {
-    moveKnight(toX, toY);
-  }
-}
-function renderSquare(i, [knightX, knightY]) {
+function renderSquare(i, knightPosition) {
   const x = i % 8;
   const y = Math.floor(i / 8);
-  const isKnightHere = x === knightX && y === knightY;
-  const black = (x + y) % 2 === 1;
-  const piece = isKnightHere ? <Knight /> : null;
-
   return (
-    <div
-      key={i}
-      style={{ width: "12.5%", height: "12.5%" }}
-      onClick={() => handleSquareClick(x, y)}
-    >
-      <Square black={black}>{piece}</Square>
+    <div key={i} style={{ width: "12.5%", height: "12.5%" }}>
+      <BoardSquare x={x} y={y}>
+        {renderPiece(x, y, knightPosition)}
+      </BoardSquare>
     </div>
   );
+}
+
+function renderPiece(x, y, [knightX, knightY]) {
+  if (x === knightX && y === knightY) {
+    return <Knight />;
+  }
 }
 
 export default function Board({ knightPosition }) {
@@ -32,15 +28,17 @@ export default function Board({ knightPosition }) {
     squares.push(renderSquare(i, knightPosition));
   }
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexWrap: "wrap",
-      }}
-    >
-      {squares}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+        }}
+      >
+        {squares}
+      </div>
+    </DndProvider>
   );
 }
---
+++ src/components/BoardSquare/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Square from "../Square";
+import { Overlay, OverlayType } from "../Overlay";
+import { canMoveKnight, moveKnight } from "../Game";
+import { ItemTypes } from "../../utils/Constants";
+import { useDrop } from "react-dnd";
+
+function BoardSquare({ x, y, children }) {
+  const black = (x + y) % 2 === 1;
+  const [{ isOver, canDrop }, drop] = useDrop(
+    () => ({
+      accept: ItemTypes.KNIGHT,
+      drop: () => moveKnight(x, y),
+      canDrop: () => canMoveKnight(x, y),
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+        canDrop: !!monitor.canDrop(),
+      }),
+    }),
+    [x, y]
+  );
+
+  return (
+    <div
+      ref={drop}
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Square black={black}>{children}</Square>
+      {isOver && !canDrop && <Overlay type={OverlayType.IllegalMoveHover} />}
+      {!isOver && canDrop && <Overlay type={OverlayType.PossibleMove} />}
+      {isOver && canDrop && <Overlay type={OverlayType.LegalMoveHover} />}
+    </div>
+  );
+}
+
+export default BoardSquare;
